f9300ac82bef1d50c3d3cd9a670df1599f21f096
leoespindula <espindulasystem.dev@gmail.com>
2023-02-09 23:06:19 -0300

    implementando API

--- CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CleanArchMvc.Aplication.DTOs;
+using CleanArchMvc.Aplication.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMvc.API.Controllers
@@ -7,5 +9,55 @@ namespace CleanArchMvc.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+            {
+                return NotFound("Não existem categorias");
+            }
+            return Ok(categories);
+        }
+        [HttpGet("{id:int}", Name = "GetCAtegory")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Essa Categoria não existe");
+            }
+            return Ok(category);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDto)
+        {
+            if (categoryDto == null)
+
+                return BadRequest("Dados Invalidos");
+
+            await _categoryService.Add(categoryDto);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDto.Id }, categoryDto);
+
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put (int id, [FromBody] CategoryDTO categoryDto)
+        {
+            if (id != categoryDto.Id)
+                return BadRequest();
+
+            if (categoryDto == null)
+                return BadRequest();
+            await _categoryService.Update(categoryDto);
+            return Ok(categoryDto);
+
+        }
     }
 }
